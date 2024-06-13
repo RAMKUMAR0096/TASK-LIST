5ea152c5d7d6e0964505ae1d8c6d8ab459db1eaf
RAM KUMAR <166999629+RAMKUMAR0096@users.noreply.github.com>
2024-06-13 13:51:26 +0530

    Update mongo.js

--- To Do Login Original/src/mongo.js
+++ To Do Login Original/src/mongo.js
@@ -1,7 +1,10 @@
 const mongoose=require("mongoose")
   
 const getdataBase=()=>{
-    mongoose.connect(process.env.MONGODB_URI)
+    mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
 .then(()=>{
     console.log('mongoose connected');
 })
